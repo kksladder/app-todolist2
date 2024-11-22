dcd8239d88ed2041d80b3b2b016acd745abd2337
kksladder <kksladder@gmail.com>
2024-11-22 13:55:03 +0900

    app-todolist2

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,20 +1,13 @@
-import React from 'react'
-import TodoItem from './TodoItem'
+import React from 'react';
+import TodoItem from './TodoItem';
 
 const TodoList = ({ mockTodoData }) => {
-  return (
-    <div>
-        <h2>할 일 목록</h2>
-        <ul>
-          {mockTodoData.map((item) => (
-            console.log(item),
-            <TodoItem key={item.id} {...item} />
-          )
-        )}
-          
-        </ul>
-      </div>
-  )
-}
+    return (
+        <div>
+            <h2>할 일 목록</h2>
+            <ul>{mockTodoData.map((item) => (console.log(item), (<TodoItem key={item.id} {...item} />)))};</ul>
+        </div>
+    );
+};
 
-export default TodoList
+export default TodoList;
